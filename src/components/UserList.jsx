108720bfec349787b5ba40a78b4d18fd59fa2810
mohs-in <in.mohsin@outlook.com>
2024-11-09 07:57:38 +0530

    changed the semantics of the users rendered

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,23 +1,27 @@
+import React from 'react';
 
 export default function UserList({users}) {
     return ( 
         <div className='mt-12 flex flex-col'>
             <h2 className='font-semibold text-xl mt-12 mx-6 mb-4'>Submitted Data</h2>
             <hr className='bg-black w-full'/>
-            <div className='grid grid-cols-3 px-4 gap-4 w-full font-semibold text-lg'>
-                <div className='p-2'>Name</div>
-                <div className='col-span-2 p-2'>Email</div>
-            </div>
-            <div className='bg-blue-100'>
-                <hr className='bg-black w-full'/>
-                {users.map((user, index) => (
-                <div key={index} className='grid grid-cols-3 px-4 gap-4 '>
-                    <div className='p-2'>{user.name}</div>
-                    <div className='col-span-2 p-2'>{user.email}</div>
-                </div>
-                ))}
-                <hr className='bg-black w-full'/>
-            </div>
+            <table>
+                <thead className='font-semibold text-lg'>
+                    <tr className='grid grid-cols-3 px-4 gap-4 '>    
+                        <td className='p-2'>Name</td>
+                        <td className='col-span-2 p-2'>Email</td>
+                    </tr>
+                </thead>
+                <tbody className='bg-blue-100' data-testid='users'>
+                    {users.map((user, index) => (
+                    <tr key={index} className='grid grid-cols-3 px-4 gap-4 '>
+                        <td className='p-2'>{user.name}</td>
+                        <td className='col-span-2 p-2'>{user.email}</td>
+                    </tr>
+                    ))}
+                </tbody>
+            </table>
+            
         </div>
     )
 }
